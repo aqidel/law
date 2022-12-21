b742eeba46b7ff68fa3c12f2069f84654410f45d
aqidel <fanofparadox@gmail.com>
2022-12-22 00:50:00 +0300

    background added

--- src/react/components/pages/Main.jsx
+++ src/react/components/pages/Main.jsx
@@ -4,7 +4,9 @@ export default function Main() {
   return (
     <main>
       <div id='homepage-bg-outer'>
-        <div id='homepage-bg-inner'></div>
+        <div id='homepage-bg-inner'>
+          <p>Мы поможем вам с покупкой недвижимости, созданием бизнеса, наследством и другими вопросами быстро и качественно.</p>
+        </div>
       </div>
     </main>
   );
